d3891bfa6a0d3ac663bba80af715f7997c2e6b49
kgryte <kgryte@gmail.com>
2014-08-31 18:22:57 -0700

    [UPDATE] remove type-checking of comparator.

--- examples/index.js
+++ examples/index.js
@@ -1,1 +1,7 @@
-var module = require( './../lib' );
+var isGreaterThan = require( './../lib' );
+
+console.log( isGreaterThan( 2, 1 ) );
+// Returns true
+
+console.log( isGreaterThan( 0, 1 ) );
+// Returns false
